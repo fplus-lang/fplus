fbbb12cd39035f3f9efa45444083ea640fe1a2e9
FreshPenguin112 <freshpenguin112@gmail.com>
2024-04-10 20:47:00 -0500

    you can pipe now waw

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const { readFileSync, writeFileSync, existsSync } = require("fs");
 const args = process.argv.slice(2);
-if (args.length === 0 || args.includes("-h") || args.includes("--help")) {
-  console.log(
-    `
+function read(x) {
+  if (
+    (args.length === 0 || args.includes("-h") || args.includes("--help"))
+  ) {
+    console.log(
+      `
 Usage: (node index.js | other run command) <input_file> [options]
   
 Options:
@@ -18,87 +22,113 @@ Examples:
   node index.js test.fpl               Compiles the fplus code to JavaScript, prints it, and does nothing else
   node index.js test.fpl -d -e -m -s   Prints the parse tree, compiles it to JavaScript, evaluates it, and saves it
 `
-  );
-  process.exit();
+    );
+    process.exit();
+  }
+  if (typeof args[0] === "string")
+    if (args[0].startsWith("-")) {
+      var stdin = process.stdin,
+        stdout = process.stdout,
+        inputChunks = "";
+
+      stdin.resume();
+      stdin.setEncoding("utf8");
+
+      stdin.on("data", function (chunk) {
+        inputChunks += chunk;
+      });
+      stdin.on("end", () => {
+        x(inputChunks);
+      });
+    } else x("");
 }
-const { readFileSync, writeFileSync } = require("fs");
-const { InputStream, CommonTokenStream } = require("antlr4");
-const { highlight } = require("cli-highlight");
-const { runInThisContext } = require("vm");
-const { format } = require("util");
-const { minify } = require("uglify-js");
-const chalk = require("chalk");
-const SimpleLangLexer = require("./lib/fplusLexer").default;
-const SimpleLangParser = require("./lib/fplusParser").default;
-const SimpleLangCompiler = require("./compiler");
+read((code) => {
+  if (code === "") {
+    code = readFileSync(String(args.length === 0 ? "" : args[0], "utf8"));
+    code = code.toString();
+  }
+  //console.log(code.toString())
 
-// Read input file
-const code = readFileSync(args[0], "utf8");
+  const { InputStream, CommonTokenStream } = require("antlr4");
+  const { highlight } = require("cli-highlight");
+  const { runInThisContext } = require("vm");
+  const { format } = require("util");
+  const { minify } = require("uglify-js");
+  const chalk = require("chalk");
+  const SimpleLangLexer = require("./lib/fplusLexer").default;
+  const SimpleLangParser = require("./lib/fplusParser").default;
+  const SimpleLangCompiler = require("./compiler");
 
-// Create compiler instance
-const input = new InputStream(code);
-const lexer = new SimpleLangLexer(input);
-const tokens = new CommonTokenStream(lexer);
-const parser = new SimpleLangParser(tokens);
-parser.buildParseTrees = true;
-const tree = parser.program();
-//console.log(args)
-if (args.includes("-d") || args.includes("--debug"))
-  console.log(tree.toStringTree(null, parser) + "\n");
-// Compile code
-const compiler = new SimpleLangCompiler(parser);
-var compiledCode = compiler.compile(tree);
-if (args.includes("-m") || args.includes("--minify")) {
-  compiledCode = minify(compiledCode, {
-    toplevel: true,
-    compress: {
-      passes: 3,
-      evaluate: true,
-    },
-    mangle: {
+  // Read input file
+  //const code = readFileSync(args[0], "utf8");
+
+  // Create compiler instance
+  const input = new InputStream(code);
+  const lexer = new SimpleLangLexer(input);
+  const tokens = new CommonTokenStream(lexer);
+  const parser = new SimpleLangParser(tokens);
+  parser.buildParseTrees = true;
+  const tree = parser.program();
+  //console.log(args)
+  if (args.includes("-d") || args.includes("--debug"))
+    console.log(tree.toStringTree(null, parser) + "\n");
+  // Compile code
+  const compiler = new SimpleLangCompiler(parser);
+  var compiledCode = compiler.compile(tree);
+  if (args.includes("-m") || args.includes("--minify")) {
+    compiledCode = minify(compiledCode, {
       toplevel: true,
-      eval: true,
-    },
-  }).code;
-}
-// Evaluate compiled code
-console.log(chalk.blue.bold("Compiled Code:\n"));
-console.log(
-  compiledCode
-    ? highlight(compiledCode, {
-        language: "js",
-      })
-    : "<blank compiled code>\n"
-);
-const e = args.includes("-e") || args.includes("--eval");
-if (e) console.log(chalk.blue.bold("Compiled Code Output:\n"));
-
-// Function to capture console output
-function captureConsoleOutput(callback) {
-  const logs = [];
-  const originalConsoleLog = console.log;
-  console.log = (...args) => {
-    logs.push(format(...args));
-  };
-  callback();
-  console.log = originalConsoleLog;
-  return logs.join("\n");
-}
-if (e) {
-  var out = captureConsoleOutput(() => runInThisContext(compiledCode));
-  console.log(out ? chalk.green(out) + "\n" : "<blank compiled code output>\n");
-}
-
-if (args.includes("-s") || args.includes("--save")) {
-  writeFileSync(`${args[0]}.js`, compiledCode);
+      compress: {
+        passes: 3,
+        evaluate: true,
+      },
+      mangle: {
+        toplevel: true,
+        eval: true,
+      },
+    }).code;
+  }
+  // Evaluate compiled code
+  console.log(chalk.blue.bold("Compiled Code:\n"));
   console.log(
-    chalk.blue.bold(`Saved Compiled Code To "${args[0]}.js"${e ? "\n" : ""}`)
+    compiledCode
+      ? highlight(compiledCode, {
+          language: "js",
+        })
+      : "<blank compiled code>\n"
   );
+  const e = args.includes("-e") || args.includes("--eval");
+  if (e) console.log(chalk.blue.bold("Compiled Code Output:\n"));
+
+  // Function to capture console output
+  function captureConsoleOutput(callback) {
+    const logs = [];
+    const originalConsoleLog = console.log;
+    console.log = (...args) => {
+      logs.push(format(...args));
+    };
+    callback();
+    console.log = originalConsoleLog;
+    return logs.join("\n");
+  }
   if (e) {
-    writeFileSync(`${args[0]}.out`, out);
+    var out = captureConsoleOutput(() => runInThisContext(compiledCode));
     console.log(
-      chalk.blue.bold(`Saved Compiled Code Output To "${args[0]}.out"`)
+      out ? chalk.green(out) + "\n" : "<blank compiled code output>\n"
     );
   }
-}
-console.log(chalk.blue.bold(`\nFinished in ${performance.now()} ms\n`));
+
+  if (args.includes("-s") || args.includes("--save")) {
+    writeFileSync(`${args[0]}.js`, compiledCode);
+    console.log(
+      chalk.blue.bold(`Saved Compiled Code To "${args[0]}.js"${e ? "\n" : ""}`)
+    );
+    if (e) {
+      writeFileSync(`${args[0]}.out`, out);
+      console.log(
+        chalk.blue.bold(`Saved Compiled Code Output To "${args[0]}.out"`)
+      );
+    }
+  }
+  console.log(chalk.blue.bold(`\nFinished in ${performance.now()} ms\n`));
+});
