3aca9a66b0d9ef5f45acd2d85e8c391bf98a97aa
FreshPenguin112 <freshpenguin112@gmail.com>
2024-04-12 14:05:10 -0500

    updates files

--- test.fpl.js
+++ test.fpl.js
@@ -1,1 +1,10 @@
-console.log("foo"),eval('console.log("this will run with eval!")'),console.log(.5<Math.random());
+if (true) {
+console.log("foo");
+
+} else {
+
+console.log("bar");
+
+}
+eval("console.log(\"this will run with eval!\")")
+console.log(Math.random() > 0.5);
